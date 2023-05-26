ab029e3acca71366d4568081da1a55aa84826710
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-26 13:40:04 +0200

    incomplete signup and retrieval of user

---
+++ src/main/java/com/kabelo/restaurant/constants/RestaurantConstants.java
@@ -0,0 +1,8 @@
+package com.kabelo.restaurant.constants;
+
+public class RestaurantConstants {
+
+    public static final String SOMETHING_WENT_WRONG= "Something Went Wrong!";
+
+    public static final String INVALID_DATA= "Invalid Data!";
+}
---
+++ src/main/java/com/kabelo/restaurant/restImpl/UserRestImpl.java
@@ -0,0 +1,30 @@
+package com.kabelo.restaurant.restImpl;
+
+import com.kabelo.restaurant.constants.RestaurantConstants;
+import com.kabelo.restaurant.rest.UserRest;
+import com.kabelo.restaurant.service.UserService;
+import com.kabelo.restaurant.utils.RestaurantUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserRestImpl implements UserRest {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+
+        try{
+            return userService.signUp(requestMap);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return RestaurantUtils.getResponseEntity(RestaurantConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
