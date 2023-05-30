3ff1c32b98b4c3e9dfc0b9c8c854cbe234d78bda
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-30 09:52:19 +0200

    validation in JwtFilter is done

--- src/main/java/com/kabelo/restaurant/JWT/JwtFilter.java
+++ src/main/java/com/kabelo/restaurant/JWT/JwtFilter.java
@@ -1,5 +1,11 @@
 package com.kabelo.restaurant.JWT;
 
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +17,42 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    Claims claims = null;
+    private String userName =null;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        if(httpServletRequest.getServletPath().matches("/user/login|user/signup|user/forgotPassword")){
+            filterChain.doFilter(httpServletRequest,httpServletResponse);
+        } else{
+            String authHeader = httpServletRequest.getHeader("Authorisation");
+            String token = null;
+
+            if(authHeader != null && authHeader.startsWith("Bearer ")){
+                token = authHeader.substring(7);
+                userName = jwtUtils.extractUsername(token);
+                claims = jwtUtils.extractAllClaims(token);
+            }
+
+            if(userName != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetails = customerUserDetailsService.loadUserByUsername(userName);
+                if(jwtUtils.validateToken(token,userDetails)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+            filterChain.doFilter(httpServletRequest,httpServletResponse);
+        }
     }
 }
